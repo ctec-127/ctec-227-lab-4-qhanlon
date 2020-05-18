0a12af0b18ceada83ce296fe42076884318ed924
qhanlon <quinn.hanlon@gmail.com>
2020-05-18 12:50:54 -0700

    Lab-5

--- gallery.php
+++ gallery.php
@@ -43,7 +43,7 @@
 		UPLOAD_ERR_INI_SIZE  		=> "Larger than upload_max_filesize.",
 		UPLOAD_ERR_FORM_SIZE 		=> "Larger than form MAX_FILE_SIZE.",
 		UPLOAD_ERR_PARTIAL 			=> "Partial upload.",
-		UPLOAD_ERR_NO_FILE 			=> "No file.",
+		UPLOAD_ERR_NO_FILE 			=> "No file was selected.",
 		UPLOAD_ERR_NO_TMP_DIR 		=> "No temporary directory.",
 		UPLOAD_ERR_CANT_WRITE 		=> "Can't write to disk.",
 		UPLOAD_ERR_EXTENSION 		=> "File upload stopped by extension.");
